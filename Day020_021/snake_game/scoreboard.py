b0a4ddc07a27feb798cc727a421c9b52592869fd
Bobby Snawder <62964973+Bsnawder85@users.noreply.github.com>
2022-01-26 15:03:54 -0500

    Added a text file to record the high scores.

--- Day020_021/snake_game/scoreboard.py
+++ Day020_021/snake_game/scoreboard.py
@@ -11,8 +11,8 @@ class Scoreboard(Turtle):
         self.goto(0, 270)
         self.color("white")
         self.score = 0
-        with open("data.txt") as file:
-            self.high_score = file.read()
+        with open("data.txt") as data:
+            self.high_score = data.read()
         self.update_score()
 
     def update_score(self):
@@ -27,6 +27,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode='w') as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
 
