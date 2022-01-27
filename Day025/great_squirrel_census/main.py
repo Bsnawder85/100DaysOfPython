92df6e1a6ab1cd5746d073fdd37e498a7624e5cd
Bobby Snawder <62964973+Bsnawder85@users.noreply.github.com>
2022-01-27 00:21:48 -0500

    Create NYC Central Park Squirrel Census project.

---
+++ Day025/great_squirrel_census/main.py
@@ -0,0 +1,9 @@
+# TODO create a table with the total amount of
+#  grey, black, and cinnamon colored squirrels from the census data.
+#  You will only need two columns: Fur Color and Count.
+#  Use Pandas to figure out how to get the counts into a new CSV table.
+
+import pandas
+
+
+
