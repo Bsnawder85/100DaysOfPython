8494f91bc78c96fdb32241724971d6359c1077f2
Bobby Snawder <62964973+Bsnawder85@users.noreply.github.com>
2022-02-10 02:54:03 -0500

    Complete the conversion app with the method to calculate miles to kilometers and display the result.

--- Day027/MilesToKilometers/main.py
+++ Day027/MilesToKilometers/main.py
@@ -5,17 +5,24 @@ window.title("Miles to Km Converter")
 window.minsize(width=300, height=150)
 window.config(padx=20, pady=20)
 
-miles_input = Entry(width=10)
+
+def calculate():
+    miles = float(miles_input.get())
+    kilometers = miles * 1.609
+    result_label["text"] = f"{kilometers}"
+
+
+miles_input = Entry(width=8)
 miles_label = Label(text="Miles")
 equal_to_label = Label(text="is equal to")
-output_label = Label(text="0")
+result_label = Label(text="0")
 km_label = Label(text="Km")
-calculate_button = Button(text="Calculate")
+calculate_button = Button(text="Calculate", command=calculate)
 
 miles_input.grid(row=0, column=1)
 miles_label.grid(row=0, column=2)
 equal_to_label.grid(row=1, column=0)
-output_label.grid(row=1, column=1)
+result_label.grid(row=1, column=1)
 km_label.grid(row=1, column=2)
 calculate_button.grid(row=2, column=1)
 
