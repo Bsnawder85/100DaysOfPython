97e74542c15defa08f7d2b39b17fdf0111f91518
Bobby Snawder <62964973+Bsnawder85@users.noreply.github.com>
2022-02-10 02:40:28 -0500

    Set up the grid of widgets in the converter GUI app.

--- Day027/MilesToKilometers/main.py
+++ Day027/MilesToKilometers/main.py
@@ -2,8 +2,21 @@ from tkinter import *
 
 window = Tk()
 window.title("Miles to Km Converter")
-window.minsize(width=400, height=200)
+window.minsize(width=300, height=150)
+window.config(padx=20, pady=20)
 
+miles_input = Entry(width=10)
+miles_label = Label(text="Miles")
+equal_to_label = Label(text="is equal to")
+output_label = Label(text="0")
+km_label = Label(text="Km")
+calculate_button = Button(text="Calculate")
 
+miles_input.grid(row=0, column=1)
+miles_label.grid(row=0, column=2)
+equal_to_label.grid(row=1, column=0)
+output_label.grid(row=1, column=1)
+km_label.grid(row=1, column=2)
+calculate_button.grid(row=2, column=1)
 
 window.mainloop()
