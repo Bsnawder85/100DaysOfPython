3eb351be7aff49cf51b48f1b501b4d4553eccb91
Bobby Snawder <62964973+Bsnawder85@users.noreply.github.com>
2022-02-11 01:34:47 -0500

    Set up the main UI components: the labels, entries, and buttons.

---
+++ Day029/PasswordManager/main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=40, pady=30)
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+website_label = Label(text="Website:")
+email_username_label = Label(text="Email/Username:")
+password_label = Label(text="Password:")
+website_entry = Entry(width=35)
+email_username_entry = Entry(width=35)
+password_entry = Entry(width=21)
+generate_password_button = Button(text="Generate Password")
+add_password_button = Button(text="Add", width=36)
+
+canvas.grid(row=0, column=1)
+website_label.grid(row=1, column=0)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_username_label.grid(row=2, column=0)
+email_username_entry.grid(row=2, column=1, columnspan=2)
+password_label.grid(row=3, column=0)
+password_entry.grid(row=3, column=1)
+generate_password_button.grid(row=3, column=2)
+add_password_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
