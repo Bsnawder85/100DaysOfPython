8a76eb9b4f01290fe5346bb2b8a26f596730cca0
Bobby Snawder <62964973+Bsnawder85@users.noreply.github.com>
2022-02-18 00:43:03 -0500

    Set up GUI

--- Day031/FlashCardApp/main.py
+++ Day031/FlashCardApp/main.py
@@ -1,2 +1,28 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+# WINDOW
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+# CANVAS
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+# IMAGES
+card_front_image = PhotoImage(file="images/card_front.png")
+card_back_image = PhotoImage(file="images/card_back.png")
+wrong_image = PhotoImage(file="images/wrong.png")
+right_image = PhotoImage(file="images/right.png")
+# FLASH CARD
+canvas.create_image(400, 263, image=card_front_image)
+canvas.create_text(400, 150, text="French", font=("Arial", 40, "italic"), justify="center")
+canvas.create_text(400, 263, text="Some French vocab", font=("Arial", 60, "bold"), justify="center")
+# BUTTONS
+right_button = Button(image=right_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+# PLACE ELEMENTS IN THE 2x2 GRID
+canvas.grid(row=0, column=0, columnspan=2)
+wrong_button.grid(row=1, column=0)
+right_button.grid(row=1, column=1)
+
+window.mainloop()
