3ad7ee83d473406a8ba81e813965166774c749eb
Bobby Snawder <62964973+Bsnawder85@users.noreply.github.com>
2022-06-25 13:42:16 -0400

    main.py of Emailer App created and set up with first test email account.

--- Day032/EmailerApp/main.py
+++ Day032/EmailerApp/main.py
@@ -1,5 +1,17 @@
 import smtplib
 
-myemail = "bsnawder100DocTest@gmail.com"
+myemail = "bsnawder100DocInfo@gmail.com"
+mypassword = "2&7ZfrKp34Q$"
+email_subject = "Hello new email account!"
+email_body = "Hello new test email account for my 100 Days of Code project."
+email_msg = f"Subject:{email_subject}\n\n{email_body}"
 
-connection = smtplib.SMTP("smtp.gmail.com")
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=myemail, password=mypassword)
+    connection.sendmail(from_addr=myemail,
+                        to_addrs="bsnawder100DocTest@gmail.com",
+                        msg=email_msg
+    )
+
